40d055dbc7d5b4d181aed4261b2a56cb538e51aa
draguleee <andredragu1699@gmail.com>
2025-09-08 22:26:11 +0300

    Updated convertTemperature()

--- script.js
+++ script.js
@@ -4,21 +4,14 @@ function convertTemperature() {
     let result, unit;
 
     switch (conversionType) {
-        case "cf":
-            result = inputValue * 9/5 + 32; unit = "°F"; break;
-        case "fc":
-            result = (inputValue - 32) * 5/9; unit = "°C"; break;
-        case "ck":
-            result = inputValue + 273.15; unit = "K"; break;
-        case "kc":
-            result = inputValue - 273.15; unit = "°C"; break;
-        case "fk":
-            result = (inputValue - 32) * 5/9 + 273.15; unit = "K"; break;
-        case "kf":
-            result = (inputValue - 273.15) * 9/5 + 32; unit = "°F"; break;
-        default:
-            document.getElementById("result").innerHTML = "Invalid!";
-            return;
+        case "cf": result = inputValue * 9/5 + 32; unit = "°F"; break;
+        case "fc": result = (inputValue - 32) * 5/9; unit = "°C"; break;
+        case "ck": result = inputValue + 273.15; unit = "K"; break;
+        case "kc": result = inputValue - 273.15; unit = "°C"; break;
+        case "fk": result = (inputValue - 32) * 5/9 + 273.15; unit = "K"; break;
+        case "kf": result = (inputValue - 273.15) * 9/5 + 32; unit = "°F"; break;
+        default: document.getElementById("result").innerHTML = "Invalid!"; return;
     }
+    
     document.getElementById("result").innerHTML = `${result.toFixed(2)} ${unit}`;
 }
